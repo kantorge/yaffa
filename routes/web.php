c9f8f9f310eb216ec4cc557d65109f2b3534f899
Gergely Kántor <kantorge@gmail.com>
2020-11-04 21:55:04 +0100

    route cleanup

--- routes/web.php
+++ routes/web.php
@@ -28,19 +28,3 @@ Route::resource('investments', 'InvestmentController');
 Route::resource('payees', 'PayeeController');
 Route::resource('tags', 'TagController');
 Route::resource('transactions', 'TransactionController');
-
-Route::get('tt', function() {
-    //$r = app('App\Http\Requests\TransactionRequest');
-    $r= new TransactionRequest([
-        //'date' => Carbon::now(),
-        'transaction_type_id' => 1,
-        'config_type' => 'transaction_detail_standard',
-        'config' => [
-            'account_from_id' => 1,
-            'account_to_id' => 11,
-            'amount_from' => 1,
-            'amount_to' => 1,
-        ]
-    ]);
-    dd($r->rules());
-});
