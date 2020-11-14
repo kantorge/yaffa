a17e64be816466178840f67c37aa222d8a4cd6c8
Gergely Kántor <kantorge@gmail.com>
2020-11-14 15:52:00 +0100

    transaction item factory adjustment

--- database/factories/TransactionItemFactory.php
+++ database/factories/TransactionItemFactory.php
@@ -9,7 +9,7 @@ use Faker\Generator as Faker;
 
 $factory->define(TransactionItem::class, function (Faker $faker) {
     return [
-        'category_id'   => Category::inRandomOrder()->first()->id,
+        'category_id'   => Category::whereNotNull('parent_id')->inRandomOrder()->first()->id,
         'amount'        => $faker->numberBetween($min = 1, $max = 100),
         'comment'       => $faker->boolean(50) ? $faker->text($maxNbChars = 191)  : null,
     ];
