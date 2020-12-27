61b6170e296cf0bed765c3d697e2d7772462791e
Gergely Kántor <kantorge@gmail.com>
2020-12-27 15:15:20 +0100

    wip

---
+++ tests/Feature/TagTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class TagTest extends TestCase
+{
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->setBaseRoute('tags');
+        $this->setBaseModel('App\Tag');
+    }
+
+    /** @test */
+    public function user_can_view_list_of_tags()
+    {
+        $response = $this->get(route("{$this->base_route}.index"));
+
+        $response->assertStatus(200);
+        //TODO: should this be a separate variable, e.g. base view
+        $response->assertViewIs("{$this->base_route}.index");
+        $response->assertSeeText('List of tags');
+    }
+
+    /** @test */
+    public function user_cannot_create_an_tag_with_missing_data()
+    {
+        $response = $this->postJson(route("{$this->base_route}.store"), ['name' => '']);
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['name']);
+    }
+
+    /** @test */
+    public function user_can_create_an_tag()
+    {
+        $this->create();
+    }
+
+    /** @test */
+    public function user_can_edit_an_existing_tag()
+    {
+        $model = $this->base_model;
+        $response = $this->get(route("{$this->base_route}.edit", $model::inRandomOrder()->first()->id));
+
+        $response->assertStatus(200);
+        //TODO: should this be a separate variable, e.g. base view
+        $response->assertViewIs("{$this->base_route}.form");
+        //TODO: is actual message need to be tested?
+        $response->assertSeeText('Modify tag');
+    }
+
+    /** @test */
+    public function user_cannot_update_an_tag_with_missing_data()
+    {
+        $model = $this->base_model;
+        $item = $model::inRandomOrder()->first()->id;
+
+        $response = $this->patchJson(route("{$this->base_route}.update", $item),
+            [
+                'id' => $item,
+                'name' => '' ,
+            ]
+        );
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['name']);
+    }
+
+    /** @test */
+    public function user_can_update_an_tag_with_proper_data()
+    {
+        $model = $this->base_model;
+        $item = $model::inRandomOrder()->first()->id;
+
+        //$newItem = factory($model)->create();
+
+        $response = $this->patchJson(route("{$this->base_route}.update", $item),
+            [
+                'id' => $item,
+                'name' => 'aa', //TODO: make this dynamic $newItem->name,
+            ]
+        );
+        $response->assertRedirect($this->base_route);
+        //TODO: make this dynamic instead of fixed 1st element
+        $response->assertSessionHas('notification_collection.0.type', 'success');
+        //TODO: is actual message need to be tested?
+        $response->assertSessionHas('notification_collection.0.message', 'Tag updated');
+
+    }
+
+    /** @test */
+    public function user_can_delete_an_existing_tag()
+    {
+        $this->destroy();
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -39,4 +39,37 @@ abstract class TestCase extends BaseTestCase
         echo ("Seed end\r\n");
 
     }
+
+    protected function create($attributes = [], $model = '', $route = '')
+    {
+        $route = $this->base_route ? "{$this->base_route}.store" : $route;
+        $model = $this->base_model ?? $model;
+
+        $attributes = raw($model, $attributes);
+
+        $response = $this->postJson(route($route), $attributes);
+        $response->assertRedirect($this->base_route ? "{$this->base_route}" : $route);
+
+        $model = new $model;
+
+        $this->assertDatabaseHas($model->getTable(), $attributes);
+
+        return $response;
+    }
+
+    protected function destroy($model = '', $route = '')
+    {
+        $this->withoutExceptionHandling();
+
+        $route = $this->base_route ? "{$this->base_route}.destroy" : $route;
+        $model = $this->base_model ?? $model;
+
+        $model = create($model);
+
+        $response = $this->deleteJson(route($route, $model->id));
+
+        $this->assertDatabaseMissing($model->getTable(), $model->toArray());
+
+        return $response;
+    }
 }
