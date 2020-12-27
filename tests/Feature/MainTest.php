6a8655281f9dc228f5f130ac2f700f3bda779bad
Gergely Kántor <kantorge@gmail.com>
2020-12-27 20:40:08 +0100

    basic tests for main view

---
+++ tests/Feature/MainTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class MainTest extends TestCase
+{
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /** @test */
+    public function user_can_view_main_page()
+    {
+        $response = $this->get(route("accounts.summary"));
+
+        $response->assertStatus(200);
+        //TODO: should this be a separate variable, e.g. base view
+        $response->assertViewIs("accounts.summary");
+        $response->assertSeeText('Account summary');
+        $response->assertSeeText('Total value');
+    }
+
+    /** @test */
+    public function user_can_view_account_history()
+    {
+        $response = $this->get(route("accounts.history", \App\Account::inRandomOrder()->first()->id));
+        //$response = $this->get(route("accounts.history", 1));
+
+        $response->assertStatus(200);
+        $response->assertViewIs("accounts.history");
+        $response->assertSeeText('Account history');
+        $response->assertSeeText('Transaction history');
+        $response->assertSeeText('Scheduled transactions');
+    }
+
+}
