c8476d67130522e12f25826294497ae0621f844f
Gergely Kántor <kantorge@gmail.com>
2021-08-14 15:14:35 +0200

    basic filtering for budget list

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -16,6 +16,55 @@
         </div>
         <!-- /.box-header -->
         <div class="box-body">
+            <div class="row">
+                <div class="col-lg-2">
+                    <div class="form-group">
+                        <label class="control-label">
+                            Schedule
+                        </label>
+                        <div>
+                            <div class="btn-group btn-group-justified" data-toggle="buttons">
+                                <label class="btn btn-primary">
+                                    <input type="radio" name="schedule" value="Yes" class="radio-inline">
+                                    Yes
+                                </label>
+                                <label class="btn btn-primary active">
+                                    <input type="radio" name="schedule" value="" class="radio-inline" checked="checked">
+                                    Any
+                                </label>
+                                <label class="btn btn-primary">
+                                    <input type="radio" name="schedule" value="No" class="radio-inline">
+                                    No
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="form-group">
+                        <label class="control-label">
+                            Budget
+                        </label>
+                        <div>
+                            <div class="btn-group btn-group-justified" data-toggle="buttons">
+                                <label class="btn btn-primary">
+                                    <input type="radio" name="budget" value="Yes" class="radio-inline">
+                                    Yes
+                                </label>
+                                <label class="btn btn-primary active">
+                                    <input type="radio" name="budget" value="" class="radio-inline" checked="checked">
+                                    Any
+                                </label>
+                                <label class="btn btn-primary">
+                                    <input type="radio" name="budget" value="No" class="radio-inline">
+                                    No
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <table class="table table-bordered table-hover no-footer" id="table"></table>
         </div>
         <!-- /.box-body -->
