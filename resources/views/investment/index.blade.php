e348f901308b14db635a76840b24741a7cb5419e
Gergely Kántor <kantorge@gmail.com>
2021-12-04 22:16:32 +0100

    investment form simplified

--- resources/views/investment/index.blade.php
+++ resources/views/investment/index.blade.php
@@ -24,4 +24,9 @@
     </div>
     <!-- /.box -->
 
+    <form id="form-delete" action="" method="POST" style="display: none;">
+        <input type="hidden" name="_method" value="DELETE">
+        @csrf
+    </form>
+
 @stop
