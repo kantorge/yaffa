0fe7c4d30f83bb27fd869583762408e85adda6d7
kantorge <kantorge@gmail.com>
2023-05-29 13:44:13 +0200

    [fix] schedule calendar header

--- resources/js/components/Widgets/ScheduleCalendar.vue
+++ resources/js/components/Widgets/ScheduleCalendar.vue
@@ -1,47 +1,47 @@
 <template>
-  <div class="card mb-4" id="widgetScheduleCalendar">
-    <div class="card-header d-flex justify-content-between">
-      <div class="card-title">
-        {{ __('Scheduled transaction instances') }}
-      </div>
-      <div>
-        <button type="button" class="btn-close" aria-label="Close" @click="hide" :disabled="busy"></button>
-      </div>
-    </div>
-    <div class="card-body">
-      <p aria-hidden="true" v-if="busy" class="placeholder-glow">
-        <span class="placeholder col-12"></span>
-      </p>
-      <Calendar
-          class="custom-calendar max-w-full"
-          :masks="masks"
-          :attributes="attributes"
-          :first-day-of-week="2"
-          :min-date="minDate"
-          :max-date="maxDate"
-          disable-page-swipe
-          is-expanded
-          trim-weeks
-          @transition-end="refreshTooltip"
-          v-if="!busy"
-      >
-        <template v-slot:day-content="{ day, attributes }">
-          <div>
-            <span class="day-label text-sm">{{ day.day }}</span>
-            <div>
-              <a
-                  v-for="item in attributes"
-                  :key="item.key"
-                  style="margin: 0 1px;"
-                  :href="getTransactionLink(item.customData.transaction_type.type, item.customData.id)"
-                  v-html="getTransactionTypeIcon(item.customData)"
-              ></a>
+    <div class="card mb-4" id="widgetScheduleCalendar">
+        <div class="card-header d-flex justify-content-between">
+            <div class="card-title">
+                {{ __('Scheduled transaction instances') }}
             </div>
-          </div>
-        </template>
-      </Calendar>
+            <div>
+                <button type="button" class="btn-close" aria-label="Close" @click="hide" :disabled="busy"></button>
+            </div>
+        </div>
+        <div class="card-body">
+            <p aria-hidden="true" v-if="busy" class="placeholder-glow">
+                <span class="placeholder col-12"></span>
+            </p>
+            <Calendar
+                    class="custom-calendar"
+                    :masks="masks"
+                    :attributes="transactions"
+                    :first-day-of-week="2"
+                    :min-date="minDate"
+                    :max-date="maxDate"
+                    disable-page-swipe
+                    expanded
+                    trim-weeks
+                    @transition-end="refreshTooltip"
+                    v-if="!busy"
+            >
+                <template v-slot:day-content="{ day, attributes }">
+                    <div>
+                        <span class="day-label text-sm">{{ day.day }}</span>
+                        <div>
+                            <a
+                                    v-for="item in attributes"
+                                    :key="item.key"
+                                    style="margin: 0 1px;"
+                                    :href="getTransactionLink(item.customData.id)"
+                                    v-html="getTransactionTypeIcon(item.customData)"
+                            ></a>
+                        </div>
+                    </div>
+                </template>
+            </Calendar>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -50,139 +50,137 @@ import * as helpers from '../../helpers';
 import {Calendar} from 'v-calendar';
 
 export default {
-  components: {
-    dataTableHelpers,
-    helpers,
-    Calendar,
-  },
-
-  props: {
-    locale: {
-      type: String,
-      default: window.YAFFA.locale,
-    }
-  },
-
-  methods: {
-    getTransactionTypeIcon: function (transaction) {
-      return dataTableHelpers.transactionTypeIcon(
-          transaction.transaction_type.type,
-          transaction.transaction_type.name,
-          this.getTransactionLabel(transaction)
-      );
+    components: {
+        dataTableHelpers,
+        helpers,
+        Calendar,
     },
-    getTransactionLink: function (type, id) {
-      return window.route('transaction.open', {transaction: id, action: 'enter'});
-    },
-    getTransactionLabel: function (transaction) {
-      if (transaction.transaction_type.type === 'standard') {
-        // Capitalize first letter of transaction type
-        const type = transaction.transaction_type.name.charAt(0).toUpperCase() + transaction.transaction_type.name.slice(1);
-        // Return constructed label
-        return type + ' ' +
-            helpers.toFormattedCurrency(transaction.config.amount_to, this.locale, transaction.transaction_currency) +
-            ' from ' + transaction.config.account_from.name +
-            ' to ' + transaction.config.account_to.name;
-      }
-    },
-    refreshTooltip: function () {
-      $('[data-toggle="tooltip"]').tooltip();
-    },
-    toFormattedCurrency(input, locale, currencySettings) {
-      return helpers.toFormattedCurrency(input, locale, currencySettings);
-    },
-    hide() {
-      $('#widgetScheduleCalendar').hide();
-    }
-  },
 
-  data() {
-    return {
-      busy: false,
-      transactions: [],
-      attributes: [],
-      masks: {
-        weekdays: 'WWW',
-      },
-      minDate: null,
-      maxDate: null,
-    }
-  },
+    props: {
+        locale: {
+            type: String,
+            default: window.YAFFA.locale,
+        }
+    },
 
-  created() {
-    this.busy = true;
-    let vue = this;
+    methods: {
+        getTransactionTypeIcon: function (transaction) {
+            return dataTableHelpers.transactionTypeIcon(
+                transaction.transaction_type.type,
+                transaction.transaction_type.name,
+                this.getTransactionLabel(transaction)
+            );
+        },
+        getTransactionLink: function (id) {
+            return window.route('transaction.open', {transaction: id, action: 'enter'});
+        },
+        getTransactionLabel: function (transaction) {
+            if (transaction.transaction_type.type === 'standard') {
+                // Capitalize first letter of transaction type
+                const type = transaction.transaction_type.name.charAt(0).toUpperCase() + transaction.transaction_type.name.slice(1);
+                // Return constructed label
+                return type + ' ' +
+                    helpers.toFormattedCurrency(transaction.config.amount_to, this.locale, transaction.transaction_currency) +
+                    ' from ' + transaction.config.account_from.name +
+                    ' to ' + transaction.config.account_to.name;
+            }
+        },
+        refreshTooltip: function () {
+            $('[data-toggle="tooltip"]').tooltip();
+        },
+        toFormattedCurrency(input, locale, currencySettings) {
+            return helpers.toFormattedCurrency(input, locale, currencySettings);
+        },
+        hide() {
+            $('#widgetScheduleCalendar').hide();
+        }
+    },
 
-    axios.get('/api/transactions/get_scheduled_items/schedule')
-        .then(function (response) {
-          vue.attributes = response.data.transactions
-              // Keep only the transactions with a next date set.
-              // Note: the date values are not converted to JavaScript Date objects in general.
-              // Note: at this point, all items should have a schedule and next date, but a double-check is performed
-              .filter((transaction) => transaction.transaction_schedule && transaction.transaction_schedule.next_date)
-              // Map the data to the format required by the calendar component
-              .map(function (transaction, index) {
-                return {
-                  key: index + 1,
-                  customData: transaction,
-                  dates: new Date(transaction.transaction_schedule.next_date)
+    data() {
+        return {
+            busy: false,
+            transactions: [],
+            masks: {
+                weekdays: 'WWW',
+            },
+            minDate: null,
+            maxDate: null,
+        }
+    },
+
+    created() {
+        this.busy = true;
+        let vue = this;
+
+        axios.get('/api/transactions/get_scheduled_items/schedule')
+            .then(function (response) {
+                vue.transactions = response.data.transactions
+                    // Keep only the transactions with a next date set.
+                    // Note: the date values are not converted to JavaScript Date objects in general.
+                    // Note: at this point, all items should have a schedule and next date, but a double-check is performed
+                    .filter((transaction) => transaction.transaction_schedule && transaction.transaction_schedule.next_date)
+                    // Map the data to the format required by the calendar component
+                    .map(function (transaction, index) {
+                        return {
+                            key: index + 1,
+                            customData: transaction,
+                            dates: new Date(transaction.transaction_schedule.next_date)
+                        }
+                    })
+
+                // Set min and max dates or fall back to current month
+                if (vue.transactions.length > 0) {
+                    vue.minDate = vue.transactions.map((transaction) => transaction.dates).reduce(function (a, b) {
+                        return (a < b ? a : b);
+                    });
+                    vue.maxDate = vue.transactions.map((transaction) => transaction.dates).reduce(function (a, b) {
+                        return (a > b ? a : b);
+                    });
+                } else {
+                    const date = new Date();
+                    vue.minDate = new Date(date.getFullYear(), date.getMonth(), 1);
+                    vue.maxDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
                 }
-              })
-
-          // Set min and max dates or fall back to current month
-          if (vue.attributes.length > 0) {
-            vue.minDate = vue.attributes.map((transaction) => transaction.dates).reduce(function (a, b) {
-              return (a < b ? a : b);
+            })
+            .finally(function () {
+                window.a = vue.transactions;
+                vue.busy = false;
+                setTimeout(() => vue.refreshTooltip(), 1000);
             });
-            vue.maxDate = vue.attributes.map((transaction) => transaction.dates).reduce(function (a, b) {
-              return (a > b ? a : b);
-            });
-          } else {
-            const date = new Date();
-            vue.minDate = new Date(date.getFullYear(), date.getMonth(), 1);
-            vue.maxDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-          }
-        })
-        .finally(function () {
-          vue.busy = false;
-          setTimeout(() => vue.refreshTooltip(), 1000);
-        });
-  },
+    },
 };
 </script>
 
 <style>
 .custom-calendar.vc-container {
-  border-radius: 0;
-  width: 100%;
+    border-radius: 0;
 }
 
 .custom-calendar .vc-header {
-  background-color: #f1f5f8;
-  padding: 10px 0;
+    margin-bottom: 10px;
 }
 
 .custom-calendar .vc-weeks {
-  padding: 0;
+    padding: 0;
 }
 
 .custom-calendar .vc-weekday {
-  background-color: #f8fafc;
-  border-bottom: 1px solid #eaeaea;
-  border-top: 1px solid #eaeaea;
-  padding: 5px 0;
+    background-color: #f8fafc;
+    border-bottom: 1px solid #eaeaea;
+    border-top: 1px solid #eaeaea;
+    padding: 5px 0;
 }
 
 .custom-calendar .vc-day {
-  border: 1px solid #b8c2cc;
-  padding: 0 5px 3px 5px;
-  text-align: left;
-  height: 60px;
-  min-width: 45px;
-  background-color: white;
+    border: 1px solid #b8c2cc;
+    padding: 0 5px 3px 5px;
+    text-align: left;
+    height: 60px;
+    min-width: 45px;
+    background-color: white;
 }
 
 .custom-calendar .vc-day-dots {
-  margin-bottom: 5px;
+    margin-bottom: 5px;
 }
 </style>
