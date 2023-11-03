5837f6dc761a474c2c06cfa6f153f5bb481b755c
kantorge <kantorge@gmail.com>
2023-11-03 22:02:14 +0100

    fix: read the app version from config.json instead of a config file

--- config/yaffa.php
+++ config/yaffa.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'version' => '0.1.0',
+    'version' => json_decode(file_get_contents(base_path('composer.json')))->version,
     'admin_email' => env('ADMIN_EMAIL', 'admin@yaffa.test'),
     'alpha_vantage_key' => env('ALPHA_VANTAGE_KEY'),
     'registered_user_limit' => env('REGISTERED_USER_LIMIT'),
