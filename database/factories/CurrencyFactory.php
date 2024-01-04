990155eb13fa8784dcaef11f223fa0f03fcf547d
kantorge <kantorge@gmail.com>
2024-01-04 21:35:25 +0100

    fix: swap currency exchange rate provider

--- database/factories/CurrencyFactory.php
+++ database/factories/CurrencyFactory.php
@@ -41,4 +41,25 @@ class CurrencyFactory extends Factory
             'user_id' => $user->id,
         ];
     }
+
+    /**
+     * Create a state where the user can provide an array of ISO codes to select from,
+     * assuming the selected currencies are also available in the currency faker provider.
+     */
+    public function fromIsoCodes(array $isoCodes): CurrencyFactory
+    {
+        return $this->state(function (array $attributes) use ($isoCodes) {
+            $isoCode = $this->faker->randomElement($isoCodes);
+            $currency = $this->faker->currencyArrayByIsoCode($isoCode);
+
+            return [
+                'name' => $currency['name'],
+                'iso_code' => $currency['iso_code'],
+                'num_digits' => $currency['num_digits'],
+                'base' => null,
+                'auto_update' => $this->faker->boolean,
+                'user_id' => $attributes['user_id'] ?? User::factory()->create()->id,
+            ];
+        });
+    }
 }
