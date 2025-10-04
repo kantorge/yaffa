8de720b7e1dc2a4edc6bec31c53f2a00c4b7c359
kantorge <kantorge@gmail.com>
2025-10-04 14:37:37 +0200

    fix: migrate dataLayer event

--- app/Listeners/DataLayerEventForLoginSuccess.php
+++ app/Listeners/DataLayerEventForLoginSuccess.php
@@ -2,23 +2,29 @@
 
 namespace App\Listeners;
 
+use App\Models\User;
+use Illuminate\Auth\Events\Login;
+
 class DataLayerEventForLoginSuccess
 {
     /**
      * Handle the event.
      */
-    public function handle(object $event): void
+    public function handle(Login $event): void
     {
         // This feature is triggered only if the GTM container ID is set
-        if (! config('yaffa.gtm_container_id')) {
+        if (!config('yaffa.gtm_container_id')) {
             return;
         }
+
+        /** @var User $user */
+        $user = $event->user;
 
         // As this event happens on the server side, we need to push the event to the dataLayer of the next page load
         $dataLayer = session()->get('dataLayer', []);
         $dataLayer[] = [
             'event' => 'loginSuccess',
-            'is_generic_demo_user' => $event->user->email === 'demo@yaffa.cc',
+            'is_generic_demo_user' => $user->email === 'demo@yaffa.cc',
         ];
         session()->flash('dataLayer', $dataLayer);
     }
