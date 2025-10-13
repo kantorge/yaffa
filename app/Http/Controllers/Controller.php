4906e2c3b8cc4f8986a7742c0491f2bfaafe66c7
Shift <shift@laravelshift.com>
2025-10-13 20:35:10 +0000

    Remove unused `ValidatesRequests` trait

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Components\FlashMessages;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 abstract class Controller extends BaseController
@@ -13,5 +12,4 @@ abstract class Controller extends BaseController
     use AuthorizesRequests;
     use DispatchesJobs;
     use FlashMessages;
-    use ValidatesRequests;
 }
