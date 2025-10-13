7090a764d699eeefcf3afdceea8ea143b4f37add
Shift <shift@laravelshift.com>
2025-10-13 20:35:19 +0000

    Remove base controller inheritance

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Components\FlashMessages;
-use Illuminate\Routing\Controller as BaseController;
 
-abstract class Controller extends BaseController
+abstract class Controller
 {
     use FlashMessages;
 }
