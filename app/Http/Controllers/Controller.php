085499a417bd559f7b21f6ec280edeca4a7c4700
Shift <shift@laravelshift.com>
2025-10-13 20:35:19 +0000

    Remove unused `DispatchesJobs` trait

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,11 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Components\FlashMessages;
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 
 abstract class Controller extends BaseController
 {
-    use DispatchesJobs;
     use FlashMessages;
 }
