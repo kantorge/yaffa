3050fb5a77e1f37f19dffb7f08e56d1cedb01872
kantorge <kantorge@gmail.com>
2026-02-10 23:22:19 +0100

    refactor(vue): update reporting and modal components for unified transaction type/config_type distinction

--- resources/js/components/ReportingWidgets/ReportingCanvas-FindTransactions-Timeline.vue
+++ resources/js/components/ReportingWidgets/ReportingCanvas-FindTransactions-Timeline.vue
@@ -1,44 +1,38 @@
 <template>
-    <div class="row mb-3">
-        <div class="col-12">
-            <monthly-timeline
-                :transactions="transactions"
-                :busy="busy"
-            ></monthly-timeline>
-        </div>
+  <div class="row mb-3">
+    <div class="col-12">
+      <monthly-timeline
+        :transactions="transactions"
+        :busy="busy"
+      ></monthly-timeline>
     </div>
+  </div>
 </template>
 
 <script>
+  import { __ } from '../../helpers';
 
-import { __ as translator } from "../../helpers";
-
-// Import all required reporting widgets
-import MonthlyTimeline from "./MonthlyTimeline.vue";
-export default {
+  // Import all required reporting widgets
+  import MonthlyTimeline from './MonthlyTimeline.vue';
+  export default {
     name: 'ReportingCanvasFindTransactionsTimeline',
     components: {
-        'monthly-timeline': MonthlyTimeline,
+      'monthly-timeline': MonthlyTimeline,
     },
     props: {
-        transactions: {
-            type: Array,
-            required: false,
-            default: () => []
-        },
-        busy: {
-            type: Boolean,
-            required: true
-        }
+      transactions: {
+        type: Array,
+        required: false,
+        default: () => [],
+      },
+      busy: {
+        type: Boolean,
+        required: true,
+      },
     },
     methods: {
-        /**
-         * Define the translation helper function locally.
-         */
-        __: function (string, replace) {
-            return translator(string, replace);
-        },
+      __,
     },
-    mounted() {}
-}
+    mounted() {},
+  };
 </script>
--- resources/js/components/TransactionDisplay/Modal.vue
+++ resources/js/components/TransactionDisplay/Modal.vue
@@ -1,101 +1,107 @@
 <template>
-    <div class="modal fade" id="modal-quickview">
-        <div class="modal-dialog modal-xl">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">
-                        {{ __('Details of transaction #:transaction', {transaction: transaction.id}) }}
-                    </h5>
-                    <button type="button" class="btn-close" data-coreui-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <transaction-show-standard
-                            v-if="transaction.transaction_type?.type === 'standard'"
-                            :transaction=transaction
-                    ></transaction-show-standard>
-                    <transaction-show-investment
-                            v-if="transaction.transaction_type?.type === 'investment'"
-                            :transaction=transaction
-                    ></transaction-show-investment>
-                </div>
-                <div class="modal-footer" v-if="transaction.id">
-                    <action-button-bar
-                            :transaction=transaction
-                            :controls=controls
-                            :is-modal=true
-                            @transactionUpdated="transactionUpdated"
-                    ></action-button-bar>
-                </div>
-            </div>
+  <div class="modal fade" id="modal-quickview">
+    <div class="modal-dialog modal-xl">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">
+            {{
+              __('Details of transaction #:transaction', {
+                transaction: transaction.id,
+              })
+            }}
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-coreui-dismiss="modal"
+            aria-label="Close"
+          ></button>
         </div>
+        <div class="modal-body">
+          <transaction-show-standard
+            v-if="transaction.config_type === 'standard'"
+            :transaction="transaction"
+          ></transaction-show-standard>
+          <transaction-show-investment
+            v-if="transaction.config_type === 'investment'"
+            :transaction="transaction"
+          ></transaction-show-investment>
+        </div>
+        <div class="modal-footer" v-if="transaction.id">
+          <action-button-bar
+            :transaction="transaction"
+            :controls="controls"
+            :is-modal="true"
+            @transactionUpdated="transactionUpdated"
+          ></action-button-bar>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import ShowStandard from './ShowStandard.vue'
-import ShowInvestment from "./ShowInvestment.vue";
-import ActionButtonBar from "./ActionButtonBar.vue";
-import { __ as translator } from "../../helpers"
+  import ShowStandard from './ShowStandard.vue';
+  import ShowInvestment from './ShowInvestment.vue';
+  import ActionButtonBar from './ActionButtonBar.vue';
+  import { __ } from '../../helpers';
 
-export default {
+  export default {
     name: 'QuickViewTransactionModal',
     components: {
-        'transaction-show-standard': ShowStandard,
-        'transaction-show-investment': ShowInvestment,
-        'action-button-bar': ActionButtonBar,
+      'transaction-show-standard': ShowStandard,
+      'transaction-show-investment': ShowInvestment,
+      'action-button-bar': ActionButtonBar,
     },
     props: {
-        initialControls: {
-            type: Object,
-            default: {
-                show: true,
-                edit: true,
-                clone: true,
-                skip: false,
-                enter: false,
-                delete: false,
-            },
+      initialControls: {
+        type: Object,
+        default: {
+          show: true,
+          edit: true,
+          clone: true,
+          skip: false,
+          enter: false,
+          delete: false,
         },
-        originalTransaction: Object,
+      },
+      originalTransaction: Object,
     },
     data() {
-        return {
-            transaction: Object.assign({}, this.originalTransaction),
-            controls: this.initialControls,
-            modal: undefined,
-        };
+      return {
+        transaction: Object.assign({}, this.originalTransaction),
+        controls: this.initialControls,
+        modal: undefined,
+      };
     },
     methods: {
-        close() {
-            this.$emit('close');
-        },
-        showTransaction(transaction, controls) {
-            this.transaction = transaction;
-            this.controls = controls;
+      close() {
+        this.$emit('close');
+      },
+      showTransaction(transaction, controls) {
+        this.transaction = transaction;
+        this.controls = controls;
 
-            this.modal.show();
-        },
-        transactionUpdated: function (transaction) {
-            this.transaction = Object.assign({}, transaction);
-        },
-
-        /**
-         * Define the translation helper function locally.
-         */
-        __: function (string, replace) {
-            return translator(string, replace);
-        },
+        this.modal.show();
+      },
+      transactionUpdated: function (transaction) {
+        this.transaction = Object.assign({}, transaction);
+      },
+      __,
     },
     mounted() {
-        let $vm = this;
+      let $vm = this;
 
-        // Set up global event listener for displaying a transaction in the modal
-        window.addEventListener('showTransactionQuickViewModal', function (event) {
-            $vm.showTransaction(event.detail.transaction, event.detail.controls);
-        });
+      // Set up global event listener for displaying a transaction in the modal
+      window.addEventListener(
+        'showTransactionQuickViewModal',
+        function (event) {
+          $vm.showTransaction(event.detail.transaction, event.detail.controls);
+        },
+      );
 
-        // Initialize modal
-        this.modal = new coreui.Modal(document.getElementById('modal-quickview'));
+      // Initialize modal
+      this.modal = new coreui.Modal(document.getElementById('modal-quickview'));
     },
-};
+  };
 </script>
