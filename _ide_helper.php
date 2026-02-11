32dc0117d89ca0605a69e5ab2fd29f20745ec651
kantorge <kantorge@gmail.com>
2026-02-11 23:20:43 +0100

    chore: composer update

--- _ide_helper.php
+++ _ide_helper.php
@@ -1610,15 +1610,11 @@ namespace Illuminate\Support\Facades {
         }
 
         /**
-         * Finds an entry of the container by its identifier and returns it.
+         * {@inheritdoc}
          *
          * @template TClass of object
          * @param string|class-string<TClass> $id
          * @return ($id is class-string<TClass> ? TClass : mixed)
-         * @param string $id Identifier of the entry to look for.
-         * @throws NotFoundExceptionInterface  No entry was found for **this** identifier.
-         * @throws ContainerExceptionInterface Error while retrieving the entry.
-         * @return mixed Entry.
          * @static
          */
         public static function get($id)
