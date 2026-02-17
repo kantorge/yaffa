6883579b9055416446b7a56b35a7977c15ba43a3
kantorge <kantorge@gmail.com>
2026-02-17 16:05:55 +0100

    feat: Adds Polish translation and language support (#408)

---
+++ lang/pl/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    "failed" => "Nieprawidłowe dane logowania.",
+    "password" => "Podane hasło jest nieprawidłowe.",
+    "throttle" => "Wyczerpano limit prób"
+];
---
+++ lang/pl/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    "reset" => "Twoje hasło zostało zresetowane!",
+    "sent" => "Wysłaliśmy link do resetowania hasła na Twój adres e-mail!",
+    "throttled" => "Poczekaj, zanim spróbujesz ponownie.",
+    "token" => "Ten token resetowania hasła jest nieprawidłowy.",
+    "user" => "Nie możemy znaleźć użytkownika o tym adresie e-mail."
+];
