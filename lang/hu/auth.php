167814d8935137981d8dfd582304b8fc3feeb296
kantorge <kantorge@gmail.com>
2026-02-22 22:01:42 +0100

    Various fixes and improvements

--- lang/hu/auth.php
+++ lang/hu/auth.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-    "failed" => "Helytelen belépési adatok."
+    "failed" => "Helytelen belépési adatok.",
+    "password" => "A megadott jelszó helytelen.",
+    "throttle" => "Túl sok bejelentkezési kísérlet. Próbáld újra :másodperc másodperc múlva."
 ];
