2989254ab509a5b3d61393d7752254ebad93a65b
kantorge <kantorge@gmail.com>
2026-07-26 10:40:22 +0200

    chore: local development settings

--- vite.config.js
+++ vite.config.js
@@ -50,6 +50,9 @@ export default defineConfig({
         },
     },
     server: {
+        host: '0.0.0.0',
+        port: Number(process.env.VITE_PORT) || 5173,
+        strictPort: true,
         hmr: {
             host: 'localhost',
         },
