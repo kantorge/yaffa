7c5cd35cd09a12d1e2167db5a64fea76c1cb8f7d
Shift <shift@laravelshift.com>
2026-08-28 05:15:22 +0000

    Shift config files

---
+++ config/cache.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Serializable Classes
+    |--------------------------------------------------------------------------
+    |
+    | This value determines the classes that can be unserialized from cache
+    | storage. By default, no PHP classes will be unserialized from your
+    | cache to prevent gadget chain attacks if your APP_KEY is leaked.
+    |
+    */
+
+    'serializable_classes' => false,
+
+];
