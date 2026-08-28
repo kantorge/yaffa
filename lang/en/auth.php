5f55265a56536691e02280d7da40796c2443dcfa
Shift <shift@laravelshift.com>
2026-08-28 05:14:19 +0000

    Slim `lang` files

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -2,6 +2,5 @@
 
 return [
     "failed" => "Invalid login credentials.",
-    "password" => "The provided password is incorrect.",
-    "throttle" => "Too many login attempts. Please try again in :seconds seconds."
-];
+
+    ];
